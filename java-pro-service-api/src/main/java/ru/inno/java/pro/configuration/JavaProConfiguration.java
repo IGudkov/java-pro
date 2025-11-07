01779bfea09a5ce74bbcd17b490397e7c401f92e
IGudkov <IGudkov@inno.tech>
2025-11-07 09:45:01 +0000

    task4 - исправление замечаний 1

--- java-pro-service-api/src/main/java/ru/inno/java/pro/configuration/JavaProConfiguration.java
+++ java-pro-service-api/src/main/java/ru/inno/java/pro/configuration/JavaProConfiguration.java
@@ -1,20 +1,36 @@
 package ru.inno.java.pro.configuration;
 
 import com.zaxxer.hikari.HikariDataSource;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.context.annotation.PropertySource;
 
 import javax.sql.DataSource;
 
 @Configuration
+@PropertySource(value = "classpath:application.properties")
 public class JavaProConfiguration {
+
+  @Value("${app.datasource.url}")
+  private String jdbcUrl;
+
+  @Value("${app.datasource.schema}")
+  private String schema;
+
+  @Value("${app.datasource.username}")
+  private String username;
+
+  @Value("${app.datasource.password}")
+  private String password;
+
   @Bean
   public DataSource dataSource() {
     HikariDataSource dataSource = new HikariDataSource();
-    dataSource.setJdbcUrl("jdbc:postgresql://localhost:5432/postgres");
-    dataSource.setSchema("java_pro");
-    dataSource.setUsername("admin");
-    dataSource.setPassword("1234");
+    dataSource.setJdbcUrl(jdbcUrl);
+    dataSource.setSchema(schema);
+    dataSource.setUsername(username);
+    dataSource.setPassword(password);
     return dataSource;
   }
 }
